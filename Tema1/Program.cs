90a849235364477a8f337223b6c486da8e91afa2
datecasp <ciudadano.fake@gmail.com>
2022-07-15 19:41:59 +0200

    Ejercicio final

--- Tema1/Program.cs
+++ Tema1/Program.cs
@@ -10,6 +10,15 @@ namespace Tema1
     {
         static void Main(string[] args)
         {
+            //Recibe nombre por consola y lo muestra
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+
+            //Coge hora de sistema y la muestra
+            string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+            Console.WriteLine(datetime);
         }
     }
+
 }
