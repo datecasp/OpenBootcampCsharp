981110ccdf0c9ed64aa971ec1b68b103e5b2c6b4
datecasp <ciudadano.fake@gmail.com>
2022-07-16 15:18:02 +0200

    first commit tema2 ej1 y ej2

--- Tema2/Program.cs
+++ Tema2/Program.cs
@@ -6,7 +6,44 @@ namespace Tema2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /************************************
+             *  Ejercicio 1
+             *  Recibe  datos de una persona y genera un mensaje,
+             *  usa una variable para cada dato y otra para el mensaje
+             *  
+             *  ***********************************/
+            string nombre;
+            string edad;
+            Console.WriteLine("Hoal usuario.\nCómo te llamas?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Cuantos años tienes?");
+            edad = Console.ReadLine();
+
+           
+        }
+        /*************************************
+            * 
+            *  Ejercicio 2
+            *  Usando los tipos de variables más adecuados, describe los objetos siguientes:
+               Coche: puertas, ruedas, marca, ITV vigente
+               Mesa: peso, largo, material, color
+           *
+           ***************************************/
+
+        class Coche
+        {
+            int puertas;
+            int ruedas;
+            string marca;
+            bool itvVigente;
+        }
+
+        class Mesa
+        {
+            float peso;
+            float largo;
+            string material;
+            string color;
         }
     }
 }
